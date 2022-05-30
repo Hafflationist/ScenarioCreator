54416b3e5318a23f269393eb2a9c0486d4dc7c73
Marcel Robohm <internetzverbindung@gmail.com>
2022-05-30 15:28:20 +0200

    feat: improves checking for valid foreign key constraints

--- src/main/java/de/mrobohm/integrity/IntegrityChecker.java
+++ src/main/java/de/mrobohm/integrity/IntegrityChecker.java
@@ -5,7 +5,9 @@ import de.mrobohm.data.column.constraint.ColumnConstraint;
 import de.mrobohm.data.column.constraint.ColumnConstraintForeignKey;
 import de.mrobohm.data.column.constraint.ColumnConstraintForeignKeyInverse;
 import de.mrobohm.data.column.nesting.Column;
-import de.mrobohm.data.column.nesting.*;
+import de.mrobohm.data.column.nesting.ColumnCollection;
+import de.mrobohm.data.column.nesting.ColumnLeaf;
+import de.mrobohm.data.column.nesting.ColumnNode;
 import de.mrobohm.utils.Pair;
 
 import java.util.stream.Stream;
@@ -20,34 +22,42 @@ public final class IntegrityChecker {
                 .flatMap(IntegrityChecker::extractConstraints)
                 .toList();
 
-        // TODO: Man könnte die ganze Sachen auch umgekehrt betrachten, denn jedes Inversteil müsste ein entsprechendes
-        // normales Teil besitzen. Wenn das normale Teil nicht vorliegt, entsteht allerdings kein Schaden an anderer Stelle
         var everythingOk = constraintIdPairStream.stream()
                 .filter(pair -> pair.second() instanceof ColumnConstraintForeignKey)
                 .allMatch(pair -> {
-            var sourceColumnId = pair.first();
-            var constraintForeignKey = (ColumnConstraintForeignKey) pair.second();
-            // searching for corresponding ColumnConstraintForeignKeyInverse:
-            return constraintIdPairStream.stream()
-                    .anyMatch(targetPair -> targetPair.first() == constraintForeignKey.foreignColumnId()
-                        && targetPair.second() instanceof ColumnConstraintForeignKeyInverse inverse
-                        && inverse.foreignColumnId() == sourceColumnId);
-        });
+                    var sourceColumnId = pair.first();
+                    var constraintForeignKey = (ColumnConstraintForeignKey) pair.second();
+                    // searching for corresponding ColumnConstraintForeignKeyInverse:
+                    return constraintIdPairStream.stream()
+                            .anyMatch(targetPair -> targetPair.first() == constraintForeignKey.foreignColumnId()
+                                    && targetPair.second() instanceof ColumnConstraintForeignKeyInverse inverse
+                                    && inverse.foreignColumnId() == sourceColumnId);
+                });
+        assert everythingOk : "Invalid foreign key constraint found! (target missing)";
 
-        assert everythingOk : "Invalid foreign key constraint found!";
+        var everythingOk2 = constraintIdPairStream.stream()
+                .filter(pair -> pair.second() instanceof ColumnConstraintForeignKeyInverse)
+                .allMatch(pair -> {
+                    var sourceColumnId = pair.first();
+                    var constraintForeignKeyInverse = (ColumnConstraintForeignKeyInverse) pair.second();
+                    // searching for corresponding ColumnConstraintForeignKeyInverse:
+                    return constraintIdPairStream.stream()
+                            .anyMatch(targetPair -> targetPair.first() == constraintForeignKeyInverse.foreignColumnId()
+                                    && targetPair.second() instanceof ColumnConstraintForeignKey inverse
+                                    && inverse.foreignColumnId() == sourceColumnId);
+                });
+        assert everythingOk2 : "Invalid foreign key constraint found! (source missing)";
     }
 
     private static Stream<Pair<Integer, ColumnConstraint>> extractConstraints(Column column) {
         var constraints = switch (column) {
             case ColumnLeaf leaf -> leaf.constraintSet().stream().map(c -> new Pair<>(leaf.id(), c));
-            case ColumnNode node ->
-                    Stream.concat(
-                            node.constraintSet().stream().map(c -> new Pair<>(node.id(), c)),
-                            node.columnList().stream().flatMap(IntegrityChecker::extractConstraints));
-            case ColumnCollection collection ->
-                    Stream.concat(
-                            collection.constraintSet().stream().map(c -> new Pair<>(collection.id(), c)),
-                            collection.columnList().stream().flatMap(IntegrityChecker::extractConstraints));
+            case ColumnNode node -> Stream.concat(
+                    node.constraintSet().stream().map(c -> new Pair<>(node.id(), c)),
+                    node.columnList().stream().flatMap(IntegrityChecker::extractConstraints));
+            case ColumnCollection collection -> Stream.concat(
+                    collection.constraintSet().stream().map(c -> new Pair<>(collection.id(), c)),
+                    collection.columnList().stream().flatMap(IntegrityChecker::extractConstraints));
         };
         return constraints
                 .filter(t2 -> t2.second() instanceof ColumnConstraintForeignKey
