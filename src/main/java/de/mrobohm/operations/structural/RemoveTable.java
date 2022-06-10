0774e6a084b8979be427555e313ce5c796d76d93
Marcel Robohm <internetzverbindung@gmail.com>
2022-06-10 15:27:56 +0200

    fix: only tables without foreign key constraints can be removed

--- src/main/java/de/mrobohm/operations/structural/RemoveTable.java
+++ src/main/java/de/mrobohm/operations/structural/RemoveTable.java
@@ -1,5 +1,8 @@
 package de.mrobohm.operations.structural;
 
+import de.mrobohm.data.column.constraint.ColumnConstraintForeignKey;
+import de.mrobohm.data.column.constraint.ColumnConstraintForeignKeyInverse;
+import de.mrobohm.data.column.nesting.Column;
 import de.mrobohm.data.table.Table;
 import de.mrobohm.operations.TableTransformation;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +11,7 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class RemoveTable implements TableTransformation {
 
@@ -26,6 +30,26 @@ public class RemoveTable implements TableTransformation {
     @Override
     @NotNull
     public Set<Table> getCandidates(Set<Table> tableSet) {
-        return tableSet;
+        return tableSet.stream()
+                .filter(t -> !doesHaveForeignKeyRelationships(t))
+                .collect(Collectors.toSet());
+    }
+
+    private boolean doesHaveForeignKeyRelationships(Table table) {
+        var ownColumnIdSet = table.columnList().stream()
+                .map(Column::id)
+                .collect(Collectors.toSet());
+        return table.columnList().stream().anyMatch(column -> {
+
+            var hasForeignKeys = column.constraintSet().stream()
+                    .filter(c -> c instanceof ColumnConstraintForeignKey)
+                    .map(c -> (ColumnConstraintForeignKey) c)
+                    .anyMatch(c -> !ownColumnIdSet.contains(c.foreignColumnId()));
+            var hasForeignKeysInverse = column.constraintSet().stream()
+                    .filter(c -> c instanceof ColumnConstraintForeignKeyInverse)
+                    .map(c -> (ColumnConstraintForeignKeyInverse) c)
+                    .anyMatch(c -> !ownColumnIdSet.contains(c.foreignColumnId()));
+            return hasForeignKeys || hasForeignKeysInverse;
+        });
     }
 }
