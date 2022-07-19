31eb63a3039756c1c4766db42363f8abb9313fab
Marcel Robohm <internetzverbindung@gmail.com>
2022-07-19 18:47:49 +0200

    test: adds more test cases for structual distance measure

--- src/test/java/de/mrobohm/heterogenity/StructuralDistanceMeasureElementaryTest.java
+++ src/test/java/de/mrobohm/heterogenity/StructuralDistanceMeasureElementaryTest.java
@@ -6,35 +6,32 @@ import de.mrobohm.data.Schema;
 import de.mrobohm.data.column.ColumnContext;
 import de.mrobohm.data.column.DataType;
 import de.mrobohm.data.column.DataTypeEnum;
+import de.mrobohm.data.column.constraint.ColumnConstraintForeignKey;
+import de.mrobohm.data.column.constraint.ColumnConstraintForeignKeyInverse;
 import de.mrobohm.data.column.nesting.Column;
+import de.mrobohm.data.column.nesting.ColumnCollection;
 import de.mrobohm.data.column.nesting.ColumnLeaf;
 import de.mrobohm.data.column.nesting.ColumnNode;
 import de.mrobohm.data.dataset.Value;
 import de.mrobohm.data.identification.IdSimple;
 import de.mrobohm.data.primitives.StringPlusNaked;
-import de.mrobohm.data.primitives.StringPlusSemantical;
 import de.mrobohm.data.table.Table;
-import de.mrobohm.processing.preprocessing.SemanticSaturation;
+import de.mrobohm.processing.integrity.IntegrityChecker;
 import de.mrobohm.processing.transformations.SingleTransformationExecuter;
 import de.mrobohm.processing.transformations.exceptions.NoColumnFoundException;
 import de.mrobohm.processing.transformations.exceptions.NoTableFoundException;
-import de.mrobohm.processing.transformations.structural.BinaryValueToTable;
-import de.mrobohm.processing.transformations.structural.RemoveColumn;
-import de.mrobohm.processing.transformations.structural.RemoveTable;
+import de.mrobohm.processing.transformations.structural.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.spi.LocaleNameProvider;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class StructuralDistanceMeasureElementaryTest {
 
     private String appendSpacesToName(String name) {
-        var spaces = "                        ";
+        var spaces = "                             ";
         return name + spaces.substring(name.length());
     }
 
@@ -59,6 +56,8 @@ class StructuralDistanceMeasureElementaryTest {
         var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
 
         // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
         System.out.println(appendSpacesToName("BinaryValueToTable:") + diff);
         Assertions.assertTrue(diff > 0);
     }
@@ -84,6 +83,8 @@ class StructuralDistanceMeasureElementaryTest {
         var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
 
         // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
         System.out.println(appendSpacesToName("RemoveColumn:") + diff);
         Assertions.assertTrue(diff > 0);
     }
@@ -109,6 +110,8 @@ class StructuralDistanceMeasureElementaryTest {
         var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
 
         // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
         System.out.println(appendSpacesToName("RemoveTable:") + diff);
         Assertions.assertTrue(diff > 0);
     }
@@ -160,6 +163,124 @@ class StructuralDistanceMeasureElementaryTest {
 
         // --- assert
         System.out.println(appendSpacesToName("Manual_ChangeGrouping:") + diff);
+        Assertions.assertTrue(diff > 0);
+    }
+
+    @Test
+    void calculateDistanceToRootAbsolute_ColumnCollectionToTable() throws NoTableFoundException, NoColumnFoundException {
+        // --- arrange
+        var name = new StringPlusNaked("name", Language.Technical);
+        var dt = new DataType(DataTypeEnum.NVARCHAR, false);
+        var vs = Set.of(new Value("Weiblein"), new Value("Männlein"));
+        var column1 = new ColumnLeaf(new IdSimple(101), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column2 = new ColumnLeaf(new IdSimple(102), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column3 = new ColumnLeaf(new IdSimple(103), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column4 = new ColumnLeaf(new IdSimple(104), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var columnCol = new ColumnCollection(new IdSimple(105), name, List.of(column3, column4), Set.of(), false);
+        var columnList = List.of((Column) column1, column2, columnCol);
+        var rootTable = new Table(new IdSimple(201), name, columnList, Context.getDefault(), Set.of());
+        var rootSchema = new Schema(new IdSimple(301), name, Context.getDefault(), Set.of(rootTable));
+        var transformation = new ColumnCollectionToTable();
+        var ste = new SingleTransformationExecuter(null);
+        var newSchema = ste.executeTransformation(rootSchema, transformation, new Random());
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
+
+        // --- act
+        var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
+
+        // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
+        System.out.println(appendSpacesToName("ColumnCollectionToTable:") + diff);
+        Assertions.assertTrue(diff > 0);
+    }
+
+    @Test
+    void calculateDistanceToRootAbsolute_GroupColumnLeafsToNode() throws NoTableFoundException, NoColumnFoundException {
+        // --- arrange
+        var name = new StringPlusNaked("name", Language.Technical);
+        var dt = new DataType(DataTypeEnum.NVARCHAR, false);
+        var vs = Set.of(new Value("Weiblein"), new Value("Männlein"));
+        var column1 = new ColumnLeaf(new IdSimple(101), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column2 = new ColumnLeaf(new IdSimple(102), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column3 = new ColumnLeaf(new IdSimple(103), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column4 = new ColumnLeaf(new IdSimple(104), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var columnList = List.of((Column) column1, column2, column3, column4);
+        var rootTable = new Table(new IdSimple(201), name, columnList, Context.getDefault(), Set.of());
+        var rootSchema = new Schema(new IdSimple(301), name, Context.getDefault(), Set.of(rootTable));
+        var transformation = new GroupColumnLeafsToNode();
+        var ste = new SingleTransformationExecuter(null);
+        var newSchema = ste.executeTransformation(rootSchema, transformation, new Random());
+
+        // --- act
+        var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
+
+        // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
+        System.out.println(appendSpacesToName("GroupColumnLeafsToNode:") + diff);
+        Assertions.assertTrue(5 >= diff);
+        Assertions.assertTrue(diff >= 2);
+    }
+
+    @Test
+    void calculateDistanceToRootAbsolute_MergeColumns() throws NoTableFoundException, NoColumnFoundException {
+        // --- arrange
+        var name = new StringPlusNaked("name", Language.Technical);
+        var dt = new DataType(DataTypeEnum.NVARCHAR, false);
+        var vs = Set.of(new Value("Weiblein"), new Value("Männlein"));
+        var column1 = new ColumnLeaf(new IdSimple(101), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column2 = new ColumnLeaf(new IdSimple(102), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column3 = new ColumnLeaf(new IdSimple(103), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var column4 = new ColumnLeaf(new IdSimple(104), name, dt, vs, ColumnContext.getDefault(), Set.of());
+        var columnList = List.of((Column) column1, column2, column3, column4);
+        var rootTable = new Table(new IdSimple(201), name, columnList, Context.getDefault(), Set.of());
+        var rootSchema = new Schema(new IdSimple(301), name, Context.getDefault(), Set.of(rootTable));
+        var transformation = new MergeColumns(true);
+        var ste = new SingleTransformationExecuter(null);
+        var newSchema = ste.executeTransformation(rootSchema, transformation, new Random());
+
+        // --- act
+        var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
+
+        // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
+        System.out.println(appendSpacesToName("MergeColumns:") + diff);
+        Assertions.assertTrue(diff > 0);
+    }
+
+    @Test
+    void calculateDistanceToRootAbsolute_TableToColumnCollection() throws NoTableFoundException, NoColumnFoundException {
+        // --- arrange
+        var name = new StringPlusNaked("Spalte", Language.Mixed);
+        var dataType = new DataType(DataTypeEnum.INT32, false);
+        var column1 = new ColumnLeaf(new IdSimple(1), name, dataType, ColumnContext.getDefault(), Set.of());
+        var ingestedColumn = new ColumnLeaf(new IdSimple(2), name, dataType.withIsNullable(true),
+                ColumnContext.getDefault(), Set.of(new ColumnConstraintForeignKey(new IdSimple(4), Set.of())));
+        var ingestingColumn = new ColumnLeaf(new IdSimple(4), name, dataType.withIsNullable(true),
+                ColumnContext.getDefault(),
+                Set.of(new ColumnConstraintForeignKeyInverse(new IdSimple(2), Set.of()))
+        );
+        var ingestingTable = new Table(
+                new IdSimple(10), name, List.of(ingestingColumn), Context.getDefault(), Set.of());
+        var ingestedTable = new Table(
+                new IdSimple(11), name, List.of(column1, ingestedColumn), Context.getDefault(), Set.of());
+        var tableSet = Set.of(ingestingTable, ingestedTable);
+        var rootSchema = new Schema(new IdSimple(15), name, Context.getDefault(), tableSet);
+        IntegrityChecker.assertValidSchema(rootSchema);
+        var transformation = new TableToColumnCollection(false);
+        var ste = new SingleTransformationExecuter(null);
+        var newSchema = ste.executeTransformation(rootSchema, transformation, new Random());
+
+        // --- act
+        var diff = StructuralDistanceMeasureElementary.calculateDistanceToRootAbsolute(rootSchema, newSchema);
+
+        // --- assert
+        IntegrityChecker.assertValidSchema(rootSchema);
+        IntegrityChecker.assertValidSchema(newSchema);
+        System.out.println(appendSpacesToName("TableToColumnCollection:") + diff);
         Assertions.assertTrue(diff > 0);
     }
 }
