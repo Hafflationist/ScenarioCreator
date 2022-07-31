b4bc82b36e442cc02bfd598c79d46f0d0edf9a36
Marcel Robohm <internetzverbindung@gmail.com>
2022-07-31 13:32:12 +0200

    feat: adds check for valid fds

--- src/main/java/de/mrobohm/processing/integrity/IntegrityChecker.java
+++ src/main/java/de/mrobohm/processing/integrity/IntegrityChecker.java
@@ -9,9 +9,12 @@ import de.mrobohm.data.column.nesting.ColumnCollection;
 import de.mrobohm.data.column.nesting.ColumnLeaf;
 import de.mrobohm.data.column.nesting.ColumnNode;
 import de.mrobohm.data.identification.Id;
+import de.mrobohm.data.table.FunctionalDependency;
+import de.mrobohm.data.table.Table;
 import de.mrobohm.utils.Pair;
 
 import java.util.Objects;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,18 +24,30 @@ public final class IntegrityChecker {
     }
 
     public static void assertValidSchema(Schema schema) {
-        var constraintIdPairStream = schema.tableSet().stream()
-                .flatMap(t -> t.columnList().stream())
-                .flatMap(IntegrityChecker::extractConstraints)
-                .toList();
+        assertUniquenessOfIds(schema);
+        assertValidForeignKeyConstraints(schema);
+        assertValidForeignKeyConstraints(schema);
+        assertValidFunctionalDependencies(schema);
+    }
 
-        var problematicConstraints = constraintIdPairStream.stream()
+    private static void assertUniquenessOfIds(Schema schema) {
+        var allIdList = IdentificationNumberCalculator.getAllIds(schema, false).toList();
+        var nonUniqueIdSet = allIdList.stream()
+                .filter(id -> allIdList.stream().filter(id2 -> Objects.equals(id2, id)).count() >= 2)
+                .collect(Collectors.toCollection(TreeSet::new));
+        assert nonUniqueIdSet.isEmpty() : "Non unique ids found: " + nonUniqueIdSet;
+    }
+
+    private static void assertValidForeignKeyConstraints(Schema schema) {
+        var constraintIdPairList = extractConstraints(schema).toList();
+
+        var problematicConstraints = constraintIdPairList.stream()
                 .filter(pair -> pair.second() instanceof ColumnConstraintForeignKey)
                 .filter(pair -> {
                     var sourceColumnId = pair.first();
                     var constraintForeignKey = (ColumnConstraintForeignKey) pair.second();
                     // searching for corresponding ColumnConstraintForeignKeyInverse:
-                    return constraintIdPairStream.stream()
+                    return constraintIdPairList.stream()
                             .noneMatch(targetPair -> targetPair.first().equals(constraintForeignKey.foreignColumnId())
                                     && targetPair.second() instanceof ColumnConstraintForeignKeyInverse inverse
                                     && inverse.foreignColumnId().equals(sourceColumnId));
@@ -46,13 +61,13 @@ public final class IntegrityChecker {
         assert problematicConstraints.isEmpty()
                 : "Invalid foreign key constraint found! (target missing: " + problematicConstraints + ")";
 
-        var problematicConstraints2 = constraintIdPairStream.stream()
+        var problematicConstraints2 = constraintIdPairList.stream()
                 .filter(pair -> pair.second() instanceof ColumnConstraintForeignKeyInverse)
                 .filter(pair -> {
                     var sourceColumnId = pair.first();
                     var constraintForeignKeyInverse = (ColumnConstraintForeignKeyInverse) pair.second();
                     // searching for corresponding ColumnConstraintForeignKeyInverse:
-                    return constraintIdPairStream.stream()
+                    return constraintIdPairList.stream()
                             .noneMatch(targetPair -> targetPair.first().equals(constraintForeignKeyInverse.foreignColumnId())
                                     && targetPair.second() instanceof ColumnConstraintForeignKey inverse
                                     && inverse.foreignColumnId().equals(sourceColumnId));
@@ -65,12 +80,12 @@ public final class IntegrityChecker {
                 .toList();
         assert problematicConstraints2.isEmpty()
                 : "Invalid foreign key constraint found! (source missing : " + problematicConstraints2 + ")";
+    }
 
-        var allIdList = IdentificationNumberCalculator.getAllIds(schema, false).toList();
-        var nonUniqueIdSet = allIdList.stream()
-                .filter(id -> allIdList.stream().filter(id2 -> Objects.equals(id2, id)).count() >= 2)
-                .collect(Collectors.toCollection(TreeSet::new));
-        assert nonUniqueIdSet.isEmpty() : "Non unique ids found: " + nonUniqueIdSet;
+    private static Stream<Pair<Id, ColumnConstraint>> extractConstraints(Schema schema) {
+        return schema.tableSet().stream()
+                .flatMap(t -> t.columnList().stream())
+                .flatMap(IntegrityChecker::extractConstraints);
     }
 
     private static Stream<Pair<Id, ColumnConstraint>> extractConstraints(Column column) {
@@ -86,5 +101,33 @@ public final class IntegrityChecker {
         return constraints
                 .filter(t2 -> t2.second() instanceof ColumnConstraintForeignKey
                         || t2.second() instanceof ColumnConstraintForeignKeyInverse);
+    }
+
+    private static void assertValidFunctionalDependencies(Schema schema) {
+        var errorReportSet = schema.tableSet().stream()
+                .flatMap(t -> t.functionalDependencySet().stream()
+                        .map(fd -> {
+                            var circle = fd.left().stream().anyMatch(fd.right()::contains);
+                            var allColumnIdInTableSet = t.columnList().stream()
+                                    .flatMap(column -> IdentificationNumberCalculator.columnToIdStream(column, false))
+                                    .collect(Collectors.toSet());
+                            var invalidRefSet = Stream
+                                    .concat(fd.right().stream(), fd.left().stream())
+                                    .filter(id -> !allColumnIdInTableSet.contains(id))
+                                    .collect(Collectors.toSet());
+                            return new FunctionalDependencyErrorReport(t, fd, circle, invalidRefSet);
+                        })
+                        .filter(fder -> fder.circle() || !fder.invalidRefSet().isEmpty()))
+                .collect(Collectors.toSet());
+        assert errorReportSet.isEmpty()
+                : "Invalid functional dependencies found! (erroneous fds : " + errorReportSet + ")";
+    }
+
+    private record FunctionalDependencyErrorReport(
+            Table table,
+            FunctionalDependency fd,
+            boolean circle,
+            Set<Id> invalidRefSet
+    ) {
     }
 }
