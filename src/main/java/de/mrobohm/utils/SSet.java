db44c117dd9eb68de068ba095578f10687481891
Marcel Robohm <internetzverbindung@gmail.com>
2022-08-05 15:53:36 +0200

    feat: powerset

--- src/main/java/de/mrobohm/utils/SSet.java
+++ src/main/java/de/mrobohm/utils/SSet.java
@@ -1,16 +1,44 @@
 package de.mrobohm.utils;
 
 import java.util.Arrays;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SSet {
     private SSet() {
     }
 
     @SafeVarargs
-    public static <T> SortedSet<T> of(T ... content) {
+    public static <T> SortedSet<T> of(T... content) {
         return Arrays.stream(content).collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public static <T> SortedSet<T> concat(SortedSet<T> set1, SortedSet<T> set2) {
+        return Stream.concat(set1.stream(), set2.stream()).collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public static <T> SortedSet<T> concat(SortedSet<T> set, Stream<T> stream) {
+        return Stream.concat(set.stream(), stream).collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public static <T> Set<SortedSet<T>> powerSet(SortedSet<T> set) {
+        assert set.size() <= 22 : "Calculating the power set would exceed the capabilities of my computer. : (";
+        var list = set.stream().toList();
+
+        var possibleIdxList = Stream
+                .iterate(0, x -> x + 1)
+                .limit(list.size())
+                .toList();
+        return Stream
+                .iterate(0L, x -> x + 1L)
+                .limit(0b1L << list.size())
+                .map(x -> possibleIdxList.stream()
+                        .filter(idx -> ((0b1L << idx) & x) > 0)
+                        .map(list::get)
+                        .collect(Collectors.toCollection(TreeSet::new)))
+                .collect(Collectors.toSet());
     }
 }
---
+++ src/test/java/de/mrobohm/utils/SSetTest.java
@@ -0,0 +1,36 @@
+package de.mrobohm.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Random;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+class SSetTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+            13, 14, 15, 16, 17, 18, 19, 20, 21, 22
+
+    })
+    void powerSet(int size) {
+        // --- Arrange
+        var random = new Random();
+        var set = random.ints()
+                .distinct()
+                .limit(size)
+                .boxed()
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        // --- Act
+        var powerSet = SSet.powerSet(set);
+
+        // --- Assert
+        Assertions.assertEquals(0b1 << set.size(), powerSet.size());
+        var validElements = powerSet.stream().allMatch(set::containsAll);
+        Assertions.assertTrue(validElements);
+    }
+}
