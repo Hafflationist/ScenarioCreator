4bdfa575d5446fe15cf7204b02c5ea499af185a0
Marcel Robohm <internetzverbindung@gmail.com>
2022-08-06 13:10:42 +0200

    feat: adds transClosure

---
+++ src/main/java/de/mrobohm/processing/fundep/FunctionalDependencyCalculator.java
@@ -0,0 +1,44 @@
+package de.mrobohm.processing.fundep;
+
+import de.mrobohm.data.identification.Id;
+import de.mrobohm.data.table.FunctionalDependency;
+import de.mrobohm.utils.Pair;
+import de.mrobohm.utils.SSet;
+import one.util.streamex.StreamEx;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public final class FunctionalDependencyCalculator {
+    private FunctionalDependencyCalculator() {
+    }
+
+    /**
+     * @param fdSet
+     * @return for each fd the right-hand side is expanded to the attribute closure of the left-hand side
+     */
+    public static SortedSet<FunctionalDependency> transClosure(SortedSet<FunctionalDependency> fdSet) {
+        return fdSet.stream()
+                .map(FunctionalDependency::left)
+                .map(attrSet -> new FunctionalDependency(attrSet, attributeClosure(attrSet, fdSet)))
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+    // In Wirklichkeit brauchen wir die abgeschlossene Hülle,
+    // wobei die linke und rechte Seite jeder funktionalen Abhängigkeit disjunkt sein müssen.
+    // Die restlichen funktionalen Abhängigkeiten bringen hier keinen Mehrwert.
+
+    public static SortedSet<Id> attributeClosure(SortedSet<Id> forColumnIdSet, SortedSet<FunctionalDependency> fdSet) {
+        return StreamEx
+                .iterate(forColumnIdSet, acc -> {
+                    var newAttributes = fdSet.stream()
+                            .filter(fd -> acc.containsAll(fd.left()))
+                            .flatMap(fd -> fd.right().stream());
+                    return SSet.concat(acc, newAttributes);
+                })
+                .pairMap(Pair::new)
+                .findFirst(pair -> pair.first().size() == pair.second().size())
+                .map(Pair::first)
+                .orElseThrow();
+    }
+}
---
+++ src/test/java/de/mrobohm/processing/fundep/FunctionalDependencyCalculatorTest.java
@@ -0,0 +1,33 @@
+package de.mrobohm.processing.fundep;
+
+import de.mrobohm.data.identification.IdSimple;
+import de.mrobohm.data.table.FunctionalDependency;
+import de.mrobohm.utils.SSet;
+import org.junit.jupiter.api.Test;
+
+class FunctionalDependencyCalculatorTest {
+
+
+    private FunctionalDependency of(int a, int b) {
+        return new FunctionalDependency(SSet.of(new IdSimple(a)), SSet.of(new IdSimple(b)));
+    }
+
+    @Test
+    void attributeClosure() {
+        // --- Arrange
+        var fdSet = SSet.of(
+                of(1,2),
+                of(2,3),
+                of(3,1),
+                of(4,5)
+        );
+        var attr = new IdSimple(1);
+
+        // --- Act
+        //var closureImp = FunctionalDependencyCalculator.attributeClosureImperative(SSet.of(attr), fdSet);
+        var closureFun = FunctionalDependencyCalculator.attributeClosure(SSet.of(attr), fdSet);
+
+        // --- Assert
+//        Assertions.assertEquals(closureFun, closureImp);
+    }
+}
