4ab52175d2d93d16b0e167ef622f78f85a3ca96b
Marcel Robohm <internetzverbindung@gmail.com>
2022-08-06 15:24:36 +0200

    test: reduces number of tests for powerset

--- src/test/java/de/mrobohm/utils/SSetTest.java
+++ src/test/java/de/mrobohm/utils/SSetTest.java
@@ -13,8 +13,7 @@ class SSetTest {
     @ParameterizedTest
     @ValueSource(ints = {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-            13, 14, 15, 16, 17, 18, 19, 20, 21, 22
-
+            // 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
     })
     void powerSet(int size) {
         // --- Arrange
