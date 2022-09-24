4bba5cf5e46b119251a7f8082e0d3e224e899a02
Marcel Robohm <internetzverbindung@gmail.com>
2022-09-24 14:05:44 +0200

    test: adds one

---
+++ src/test/java/de/mrobohm/data/column/constraint/regexy/RegularExpressionTest.java
@@ -0,0 +1,33 @@
+package de.mrobohm.data.column.constraint.regexy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RegularExpressionTest {
+
+    @Test
+    void toStringTest() {
+        // --- Arrange
+        final var regexTree = new RegularKleene(
+                new RegularConcatenation(
+                        new RegularSum(
+                                new RegularTerminal('a'),
+                                new RegularKleene(
+                                        new RegularConcatenation(
+                                                new RegularTerminal('b'),
+                                                new RegularTerminal('c')
+                                        )
+                                )
+                        ),
+                        new RegularTerminal('d')
+                )
+        );
+        final var regexStringExpected = "(a|((bc)*)d)*";
+
+        // --- Act
+        var regexStringActual = regexTree.toString();
+
+        // --- Assert
+        Assertions.assertEquals(regexStringExpected, regexStringActual);
+    }
+}
