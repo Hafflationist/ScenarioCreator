7e2f198b700993b0d6c50915732e40cb6d60f8b1
hafflationist <internetzverbindung(an)gmail(punkt)com>
2023-06-16 16:57:26 +0200

    chore: replaces exceptions with comments

--- ScenarioCreator.Generation/src/scenarioCreator/generation/processing/transformations/TgdHelper.java
+++ ScenarioCreator.Generation/src/scenarioCreator/generation/processing/transformations/TgdHelper.java
@@ -20,7 +20,9 @@ public class TgdHelper {
                         .map(t2 -> new Pair<>(t1,t2)))
                 .filter(Optional::isPresent)
                 .map(Optional::get);
-        throw new RuntimeException("Implement me!");
+        //throw new RuntimeException("Implement me!");
+        // TODO: Implement me!
+        return List.of();
     }
 
     private static boolean areTableEquals(Table t1, Table t2) {
